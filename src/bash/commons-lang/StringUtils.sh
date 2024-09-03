0b83b344a8f3c0e1b416dfdcb8f593d8bb1b88e2
Benjamin Vargin <benjamin.vargin.pro@proton.me>
2024-09-03 17:09:13 +0200

    Add substring.

--- src/bash/commons-lang/StringUtils.sh
+++ src/bash/commons-lang/StringUtils.sh
@@ -97,21 +97,50 @@ StringUtils.indexOf() {
     return $FUNCTION_FAILURE
   fi
 
+  # Case when researh is empty but not the research
+  if [[ -n "$inString" &&  -z "$research" ]]; then
+    echo "0" && return $FUNCTION_SUCCESS
+    return $FUNCTION_FAILURE
+  fi
+
+  # Case when both are empty
+  if [[ -z "$inString" &&  -z "$research" ]]; then
+    echo "0" && return $FUNCTION_SUCCESS
+    return $FUNCTION_FAILURE
+  fi  
+
   # Find character index in string (preffered to expr: SC2308)
+  # If the number of characters remain the same, it means that no 
+  # occurence has been found.
   index=${inString%%"${research}"*}
-  index=$((${#index}))
+  [ ${#index} -eq ${#inString} ] && index=-1 || index=$((${#index}))
 
   echo "$index" && return $FUNCTION_SUCCESS
   return $FUNCTION_FAILURE
 }
 
 
+
+# @description Search a string to find the first index 
+#              of any string in the given set of string.
+# @example
+#
+# @arg $1 string Input string to check
+# @arg $2 array  The list of string to research
+#
+# @stdout The first minimum of index the matching research, -1 if no match
+#         or empty string input or empty research
+# @stderr None
+#
+# @exitcode 0 If the index can be displayed
+# @exitcode 1 Otherwise
+# see https://stackoverflow.com/questions/10953833/passing-multiple-distinct-arrays-to-a-shell-function
 StringUtils.indexOfAny() {
   local inString="$1"
-  local research="$2"
+  local -n research="$2" 2> /dev/null || local research=()
   local index=-1
   local i
-  
+
   if [[ ${#research[@]} -eq 0 ]]; then
     echo "$index" && return $FUNCTION_SUCCESS
     return $FUNCTION_FAILURE
@@ -119,7 +148,7 @@ StringUtils.indexOfAny() {
 
   for r in "${research[@]}"; do
     i=$(StringUtils.indexOf "$inString" "$r")
-    [[ $i -ge 0 && $i -lt $index ]] && index=$i
+    [[ $i -ge 0  && ($index -eq -1 || $i -lt $index) ]] && index=$i
   done
 
   echo "$index" && return $FUNCTION_SUCCESS
@@ -194,5 +223,49 @@ StringUtils.trim() {
 
   trimmedString="$(echo "$inString" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
   echo "$trimmedString" && return $FUNCTION_SUCCESS
+  return $FUNCTION_FAILURE
+}
+
+# @description Gets a substring from the specified String.
+# @example
+#    StringUtils.trim ""                 # return ""
+#    StringUtils.trim "     "            # return ""
+#    StringUtils.trim "  Hello  World  " # return "Hello  World"
+#
+# @arg $1 the String to get the substring from
+# @arg $2 the position to start from, negative means count back from the end of the String by this many characters
+# @arg $3 the position to end at (exclusive), negative means count back from the end of the String by this many characters
+#
+# @stdout substring from start position to end position, empty if empty String input
+# @stderr None
+#
+# @exitcode 0 If the string can be displayed.
+# @exitcode 1 Otherwise.
+StringUtils.substring() {
+  local inString="$1"
+  local start=${2:-0}
+  local end=${3:-${#inString}}
+
+  # Consider negative position relative to the end of the input string
+  [[ $start -lt 0 ]] && start=$((${#inString}+start))
+  [[ $end -lt 0 ]]   && end=$((${#inString}+end))
+
+  # If still negative, then consider to 0
+  [[ $start -lt 0 ]] && start=0
+  [[ $end -lt 0 ]] && end=0
+
+  # If greater than the number of char in the string then get the length
+  [[ $start -gt ${#inString} ]] && start=${#inString}
+  [[ $end -gt ${#inString} ]] && end=${#inString}
+
+  # If same index, or start greater than end, then return empty
+  if [[ $start -ge $end ]]; then
+    echo "" && return $FUNCTION_SUCCESS
+    return $FUNCTION_FAILURE
+  fi
+
+  local substring=${inString:$start:$((end - start))}
+
+  echo "$substring" && return $FUNCTION_SUCCESS
   return $FUNCTION_FAILURE
 }
---
+++ test/bash/commons-lang/StringUtils/substring.bats
@@ -0,0 +1,52 @@
+#!/usr/bin/env bats
+
+source $( dirname "$BATS_TEST_FILENAME" )/../../../../src/bash/commons-lang/StringUtils.sh
+
+@test "substring returns "" when no arguments " {
+  run StringUtils.indexOfAny
+}
+
+@test "substring returns "" when the input argument is empty" {
+  run StringUtils.substring ""
+  [ "$output" = "" ]
+
+  run StringUtils.substring "" 0
+  [ "$output" = "" ]
+
+  run StringUtils.substring "" 0 1
+  [ "$output" = "" ]
+}
+
+@test "substring returns correct substring for nominal cases" {
+  run StringUtils.substring "abc" 0 2
+  [ "$output" = "ab" ]
+
+  run StringUtils.substring "abc" 2 4
+  [ "$output" = "c" ]
+
+  run StringUtils.substring "abc" -2 -1
+  [ "$output" = "b" ]
+
+  run StringUtils.substring "abc" -4 2
+  [ "$output" = "ab" ]
+}
+
+@test "substring returns empty when the indexes are equal" {
+  run StringUtils.substring "abc" 2 2
+  [ "$output" = "" ]
+}
+
+@test "substring returns empty when start is greater than end" {
+  run StringUtils.substring "abc" 2 0
+  [ "$output" = "" ]
+}
+
+@test "substring returns empty when start is greater than string" {
+  run StringUtils.substring "abc" 4 6
+  [ "$output" = "" ]
+}
+
+@test "substring returns empty when end is less than 0" {
+  run StringUtils.substring "abc" -4 -6
+  [ "$output" = "" ]
+}
