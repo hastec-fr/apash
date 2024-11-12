42c113582d32b8042bcf29423ff4e846ae01690e
Benjamin Vargin <benjamin.vargin@hastec.fr>
2024-11-12 15:29:57 +0100

    Add possibility to clone another version than the main.

--- utils/install.sh
+++ utils/install.sh
@@ -8,17 +8,23 @@ die() {
   exit 1
 }
 
-## stop if apash is already installed
+## stop if apash is already installed.
 [[ -d "$HOME/.apash" ]] && die "apash is already installed on [$HOME/.apash]"
 
-## stop if git is not installed
+## stop if git is not installed.
 git version >/dev/null 2>&1 || die "git is not installed on this machine"
 
-## install the project on ~/.apash
+## install the project on ~/.apash.
 echo ". Download apash code to $HOME/.apash"
-git clone https://github.com/hastec-fr/apash.git "$HOME/.apash" 2> /dev/null
 
-## now check what shell is running
+# Add capability to point on a desired git revision.
+if [ -n "$APASH_INSTALL_VERSION" ]; then
+  git clone -b "$APASH_INSTALL_VERSION" https://github.com/hastec-fr/apash.git "$HOME/.apash" 2> /dev/null
+else
+  git clone https://github.com/hastec-fr/apash.git "$HOME/.apash" 2> /dev/null
+fi
+
+## Now check what shell is running.
 if [ -n "$BASH_VERSION" ]; then    
   shell_type="bash"
   shell_version="$BASH_VERSION"
@@ -33,21 +39,21 @@ zsh)   startup_script="$HOME/.zshrc"  ;;
 *)     startup_script=""              ;;
 esac
 
-## startup script should exist already, if not then remove new cloned directory
+## Startup script should exist already, if not then remove new cloned directory.
 if [[ -n "$startup_script" && ! -f "$startup_script" ]]; then
   rm -r "$HOME/.apash"
   die "startup script [$startup_script] does not exist"
 fi
 
-## apash_keyword will allow us to remove the lines upon uninstall
+## Apash_keyword will allow us to remove the lines upon uninstall.
 apash_keyword="apashInstallTag"
 
-## now add the apash initialisation lines to the user's startup script
+## Now add the apash initialisation lines to the user's startup script.
 echo ". Add apash initialisation to [$startup_script]"
 (
   echo ". \"\$HOME/.apash/.apashrc\"         ##$apash_keyword"
   echo ". \"\$APASH_HOME_DIR/apash\" source  ##$apash_keyword"
 ) >>"$startup_script"
 
-## script is finished
+## Script is finished.
 echo "Apash is installed - open a new terminal window to start using it"
